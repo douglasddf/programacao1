739ab4b4c536dd056f88c4874238fd7f632dbf3d
Douglas Frari <douglas.frari@gmail.com>
2014-04-01 23:23:22 -0300

    Alterando classe da aula 2.

---
+++ Aula02/src/BemVindo1.java
@@ -0,0 +1,16 @@
+/*
+    Esse programa exibe uma saída de dados simples.
+
+    @author douglasdanieldelfrari
+ */
+public class BemVindo1 {
+    
+    public static void main(String[] args) 
+    { // inicio
+        
+        // comando de saida de dados TEXTUAL
+        System.out.println("Olá Mundo!");
+        
+    } // fim
+    
+}
